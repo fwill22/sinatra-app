0b95290456353d238b376fd6f85e9f0c41a3bf77
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-12 15:56:53 +0100

    Kitten with a border

--- app.rb
+++ app.rb
@@ -12,3 +12,9 @@ require "sinatra/reloader" if development?
   get '/reload' do
     "🔫"
   end
+
+  get '/cat' do
+    "<div>
+      <img class='cat' src='https://i.imgur.com/jFaSxym.png' alt='A cute kitten' style='border: 3px dashed red'>
+    </div>"
+  end
